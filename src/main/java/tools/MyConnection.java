5429a331a0f2f1baf87b51b2cfd02c2190ecf356
Malek feki <malekfeki18@gmail.com>
2024-02-18 17:02:56 +0100

    db

--- src/main/java/tools/MyConnection.java
+++ src/main/java/tools/MyConnection.java
@@ -1,4 +1,60 @@
 package tools;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+    public String url="jdbc:mysql://localhost:3306/sportify";
+    public String login="root";
+    public String password="";
+    Connection cnx;
+    public static MyConnection instance;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Connection getCnx() {
+        return cnx;
+    }
+
+    public void setCnx(Connection cnx) {
+        this.cnx = cnx;
+    }
+
+    private MyConnection() {
+        try {
+            cnx= DriverManager.getConnection(url,login,password);
+            System.out.println("Connexion établie!");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public static MyConnection getInstance(){
+        if(instance==null){
+            instance=new MyConnection();
+        }
+        return instance;
+    }
 }
